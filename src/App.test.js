04a614c820c3eada9ae24b5d88a6f26c8cdf64ce
jkanbu <jk.anbu@gmail.com>
2026-05-08 16:45:25 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -7,7 +7,7 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders learn react new link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
